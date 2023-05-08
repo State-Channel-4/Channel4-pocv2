cda4fc6a268df980dcb7d6d1f0fc8442697f6682
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-05-09 00:13:11 +0900

    feat: :lipstick: Change font to `DM Sans`

--- apps/poc-next/lib/fonts.ts
+++ apps/poc-next/lib/fonts.ts
@@ -1,11 +1,16 @@
-import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
+import {
+  JetBrains_Mono as FontMono,
+  DM_Sans as FontSans,
+} from "next/font/google"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  weight: ["400", "500", "700"],
 })
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+  weight: ["400", "500", "700"],
 })
