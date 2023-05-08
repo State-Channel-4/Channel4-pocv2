65442ec0184df433f62166c9e3b3e3d436e2bd5c
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-05-09 00:10:13 +0900

    feat: :bento: Add project logo

--- apps/poc-next/components/icons.tsx
+++ apps/poc-next/components/icons.tsx
@@ -1,11 +1,8 @@
-import {
-  Laptop,
-  LucideProps,
-  Moon,
-  SunMedium,
-  Twitter,
-  type Icon as LucideIcon,
-} from "lucide-react"
+import { Laptop, LucideProps, Moon, SunMedium, Twitter, type Icon as LucideIcon } from "lucide-react";
+
+
+
+
 
 export type Icon = LucideIcon
 
@@ -14,11 +11,128 @@ export const Icons = {
   moon: Moon,
   twitter: Twitter,
   logo: (props: LucideProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-      <path
-        fill="currentColor"
-        d="M11.572 0c-.176 0-.31.001-.358.007a19.76 19.76 0 0 1-.364.033C7.443.346 4.25 2.185 2.228 5.012a11.875 11.875 0 0 0-2.119 5.243c-.096.659-.108.854-.108 1.747s.012 1.089.108 1.748c.652 4.506 3.86 8.292 8.209 9.695.779.25 1.6.422 2.534.525.363.04 1.935.04 2.299 0 1.611-.178 2.977-.577 4.323-1.264.207-.106.247-.134.219-.158-.02-.013-.9-1.193-1.955-2.62l-1.919-2.592-2.404-3.558a338.739 338.739 0 0 0-2.422-3.556c-.009-.002-.018 1.579-.023 3.51-.007 3.38-.01 3.515-.052 3.595a.426.426 0 0 1-.206.214c-.075.037-.14.044-.495.044H7.81l-.108-.068a.438.438 0 0 1-.157-.171l-.05-.106.006-4.703.007-4.705.072-.092a.645.645 0 0 1 .174-.143c.096-.047.134-.051.54-.051.478 0 .558.018.682.154.035.038 1.337 1.999 2.895 4.361a10760.433 10760.433 0 0 0 4.735 7.17l1.9 2.879.096-.063a12.317 12.317 0 0 0 2.466-2.163 11.944 11.944 0 0 0 2.824-6.134c.096-.66.108-.854.108-1.748 0-.893-.012-1.088-.108-1.747-.652-4.506-3.859-8.292-8.208-9.695a12.597 12.597 0 0 0-2.499-.523A33.119 33.119 0 0 0 11.573 0zm4.069 7.217c.347 0 .408.005.486.047a.473.473 0 0 1 .237.277c.018.06.023 1.365.018 4.304l-.006 4.218-.744-1.14-.746-1.14v-3.066c0-1.982.01-3.097.023-3.15a.478.478 0 0 1 .233-.296c.096-.05.13-.054.5-.054z"
-      />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="25"
+      fill="none"
+      viewBox="0 0 23 25"
+    >
+      <rect
+        width="1.698"
+        height="24"
+        x="9.994"
+        fill="url(#paint0_linear_434_2015)"
+        rx="0.849"
+      ></rect>
+      <rect
+        width="1.697"
+        height="24.014"
+        fill="url(#paint1_linear_434_2015)"
+        rx="0.849"
+        transform="rotate(-120.019 5.834 9.129) skewX(-.037)"
+      ></rect>
+      <rect
+        width="1.697"
+        height="24.014"
+        fill="url(#paint2_linear_434_2015)"
+        rx="0.849"
+        transform="rotate(120.019 5.83 14.903) skewX(.037)"
+      ></rect>
+      <ellipse
+        cx="20.262"
+        cy="1.384"
+        fill="#FAFBFC"
+        rx="1.385"
+        ry="1.384"
+      ></ellipse>
+      <ellipse
+        cx="20.262"
+        cy="1.384"
+        fill="url(#paint3_radial_434_2015)"
+        rx="1.385"
+        ry="1.384"
+      ></ellipse>
+      <ellipse
+        cx="1.385"
+        cy="22.629"
+        fill="#FAFBFC"
+        rx="1.385"
+        ry="1.384"
+      ></ellipse>
+      <ellipse
+        cx="1.385"
+        cy="22.629"
+        fill="url(#paint4_radial_434_2015)"
+        rx="1.385"
+        ry="1.384"
+      ></ellipse>
+      <defs>
+        <linearGradient
+          id="paint0_linear_434_2015"
+          x1="10.843"
+          x2="10.843"
+          y1="0"
+          y2="24"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#8F00FF"></stop>
+          <stop offset="0.24" stopColor="#E5F344"></stop>
+          <stop offset="0.474" stopColor="#37FF4B"></stop>
+          <stop offset="0.719" stopColor="#4D89FF"></stop>
+          <stop offset="1" stopColor="#FB2BFF"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_434_2015"
+          x1="0.849"
+          x2="0.849"
+          y1="0"
+          y2="24.014"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#8F00FF"></stop>
+          <stop offset="0.24" stopColor="#E5F344"></stop>
+          <stop offset="0.474" stopColor="#37FF4B"></stop>
+          <stop offset="0.719" stopColor="#4D89FF"></stop>
+          <stop offset="1" stopColor="#FB2BFF"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_434_2015"
+          x1="0.849"
+          x2="0.849"
+          y1="0"
+          y2="24.014"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#8F00FF"></stop>
+          <stop offset="0.24" stopColor="#E5F344"></stop>
+          <stop offset="0.474" stopColor="#37FF4B"></stop>
+          <stop offset="0.719" stopColor="#4D89FF"></stop>
+          <stop offset="1" stopColor="#FB2BFF"></stop>
+        </linearGradient>
+        <radialGradient
+          id="paint3_radial_434_2015"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientTransform="matrix(0 1.38415 -1.38519 0 20.262 1.384)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.359" stopColor="#E5F344"></stop>
+          <stop offset="1" stopColor="#37FF4B"></stop>
+        </radialGradient>
+        <radialGradient
+          id="paint4_radial_434_2015"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientTransform="matrix(0 1.38415 -1.38519 0 1.385 22.63)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.333" stopColor="#37FF4B"></stop>
+          <stop offset="0.781" stopColor="#E5F344"></stop>
+        </radialGradient>
+      </defs>
     </svg>
   ),
   gitHub: (props: LucideProps) => (
