11fe515d3996a67d18d57ac2d517bbb89e4228c8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-05-10 18:31:59 +0900

    fix: :label: Fix `Tag` type

--- frontend/src/components/TagList.tsx
+++ frontend/src/components/TagList.tsx
@@ -1,49 +1,51 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { TagType } from '../types'
+import { Tag } from "../types";
 
 type TagListProps = {
-    tags: {
-      tags: TagType[]
-    }
-    title: string
-}
+    tags: Tag[];
+    title: string;
+};
 
 const TagList = ({ tags, title }: TagListProps) => {
-  const [selectedTags, setSelectedTags] = useState<TagType[]>([])
-  const navigate = useNavigate()
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log(selectedTags)
-  }, [selectedTags])
+    useEffect(() => {
+        console.log(selectedTags);
+    }, [selectedTags]);
 
-  const handleCheckedTags = (tagId: TagType) => {
-    if (selectedTags.includes(tagId)) {
-      setSelectedTags(selectedTags.filter((t) => t !== tagId))
-    } else {
-      setSelectedTags([...selectedTags, tagId])
-    }
-  }
+    const handleCheckedTags = (tagId: Tag) => {
+        if (selectedTags.includes(tagId)) {
+            setSelectedTags(selectedTags.filter((t) => t !== tagId));
+        } else {
+            setSelectedTags([...selectedTags, tagId]);
+        }
+    };
 
-  return (
-    <div className="tag-list">
-      <h2>{title}</h2>
-      {tags.tags.map((tag, index) => (
-        <div className="tag-preview" key={`${tag._id}-${index}`}>
-          <input
-            type="checkbox"
-            checked={selectedTags.includes(tag)}
-            onChange={() => handleCheckedTags(tag)}
-          />
-          <label>{tag.name}</label>
+    return (
+        <div className="tag-list">
+            <h2>{title}</h2>
+            {tags.map((tag, index) => (
+                <div className="tag-preview" key={`${tag._id}-${index}`}>
+                    <input
+                        type="checkbox"
+                        checked={selectedTags.includes(tag)}
+                        onChange={() => handleCheckedTags(tag)}
+                    />
+                    <label>{tag.name}</label>
+                </div>
+            ))}
+            <p>Please select at least one tag to discover content</p>
+            <button
+                onClick={() => {
+                    navigate("/discover", { state: { taglist: selectedTags } });
+                }}
+            >
+                discover
+            </button>
         </div>
-      ))}
-      <p>Please select at least one tag to discover content</p>
-      <button onClick={() => {
-        navigate("/discover", { state: { taglist: selectedTags } })
-      }}>discover</button>
-    </div>
-  )
-}
+    );
+};
 
 export default TagList
