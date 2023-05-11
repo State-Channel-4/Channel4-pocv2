29102fea0d4492059bd0ceebe3f4c315eb3574b8
hackertron <cooljay.gupta@gmail.com>
2023-05-11 11:32:46 +0200

    updated smart contract with tags and fetching data

--- contracts/UrlContract.sol
+++ contracts/UrlContract.sol
@@ -4,117 +4,123 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-// This contract allows users to submit URLs and upvote or downvote them
 contract UrlContract {
-    // The mapping of URLs to their metadata
-    mapping(string => URL) public urlToMetadata;
-
-    // The array of all submitted URLs, sorted by vote count
-    URL[] public urlArray;
-
-    // Struct to store metadata for a URL
+    
     struct URL {
+        string title;
         string url;
-        int voteCount;
+        address submittedBy;
+        uint256 upvotes;
+        uint256 downvotes;
+        string[] tagIds;
     }
-
-    // update manager
-    function update_fields(
-        string[] memory upvotes,
-        string[] memory downvotes,
-        string[] memory submit_urls
-    ) public {
-        for (uint i = 0; i < upvotes.length; i++) {
-            upvoteURL(upvotes[i]);
-        }
-        for (uint i = 0; i < downvotes.length; i++) {
-            downvoteURL(downvotes[i]);
-        }
-        for (uint i = 0; i < submit_urls.length; i++) {
-            submitURL(submit_urls[i]);
-        }
+    
+    struct Tag {
+        string name;
+        address createdBy;
+        uint256[] urlIds;
     }
-
-    // Function to submit a new URL
-    function submitURL(string memory _url) public {
-        // Check if the URL has already been submitted
-        require(
-            keccak256(bytes(urlToMetadata[_url].url)) == keccak256(bytes("")),
-            "URL has already been submitted"
-        );
-
-        // Create a new URL struct and store it in the mapping
-        urlToMetadata[_url] = URL(_url, 0);
-
-        // Add the URL to the array of all submitted URLs
-        urlArray.push(urlToMetadata[_url]);
+    
+    struct User {
+        uint256[] upvotedURLs;
+        uint256[] downvotedURLs;
+        uint256[] submittedURLs;
     }
-
-    // Function to upvote a URL
-    function upvoteURL(string memory _url) public {
-        // Increment the vote count for the URL
-        urlToMetadata[_url].voteCount++;
-
-        // Re-sort the array of URLs by vote count
-        sortURLArray();
-    }
-
-    // function to return upvote/downvote count of a URL
-    function getcount(string memory _url) public view returns (int) {
-        // return votecount
-        return urlToMetadata[_url].voteCount;
-    }
-
-    // Function to downvote a URL
-    function downvoteURL(string memory _url) public {
-        // Decrement the vote count for the URL
-        urlToMetadata[_url].voteCount--;
-
-        // Re-sort the array of URLs by vote count
-        sortURLArray();
-    }
-
-    // Helper function to sort the array of URLs by vote count
-    function sortURLArray() private {
-        // Sort the array in place using a bubble sort algorithm
-        for (uint i = 0; i < urlArray.length; i++) {
-            for (uint j = i + 1; j < urlArray.length; j++) {
-                if (urlArray[i].voteCount < urlArray[j].voteCount) {
-                    // Swap the URLs
-                    URL memory temp = urlArray[i];
-                    urlArray[i] = urlArray[j];
-                    urlArray[j] = temp;
-                }
+    
+    URL[] private urls;
+    Tag[] private tags;
+    mapping (address => User) private users;
+    
+    function createTagIfNotExists(string memory tagName) internal returns (uint256) {
+        for (uint256 i = 0; i < tags.length; i++) {
+            if (keccak256(bytes(tags[i].name)) == keccak256(bytes(tagName))) {
+                return i;
             }
         }
+        Tag memory newTag = Tag(tagName, msg.sender, new uint256[](0));
+        tags.push(newTag);
+        return tags.length - 1;
     }
-
-    // Function to return the top 10 URLs
-    function getTopURLs() public view returns (string[] memory) {
-        // Return a slice of the array with the top 10 URLs
-        string[] memory top10 = new string[](urlArray.length);
-        for (uint i = 0; i < 10; i++) {
-            top10[i] = urlArray[i].url;
+    
+    function submitURL(string memory title, string memory url, string[] memory tagIds) public {
+        require(bytes(title).length > 0 && bytes(url).length > 0 && tagIds.length > 0, "Invalid input");
+        uint256[] memory tagIndexes = new uint256[](tagIds.length);
+        for (uint256 i = 0; i < tagIds.length; i++) {
+            tagIndexes[i] = createTagIfNotExists(tagIds[i]);
         }
-        return top10;
-    }
-
-    // return all urls
-    function allURLs() public view returns (string[] memory) {
-        string[] memory all_urls = new string[](urlArray.length);
-        for (uint i = 0; i < urlArray.length; i++) {
-            all_urls[i] = urlArray[i].url;
+        URL memory newURL = URL(title, url, msg.sender, 0, 0, tagIds);
+        urls.push(newURL);
+        for (uint256 i = 0; i < tagIndexes.length; i++) {
+            tags[tagIndexes[i]].urlIds.push(urls.length - 1);
         }
-        return all_urls;
+    }
+    
+    function getURLsByTag(string memory tagId) public view returns (URL[] memory) {
+        bool tagFound = false;
+        uint256[] memory urlIds = new uint256[](0);
+        for (uint256 i = 0; i < tags.length; i++) {
+            if (keccak256(bytes(tags[i].name)) == keccak256(bytes(tagId))) {
+                tagFound = true;
+                urlIds = tags[i].urlIds;
+                break;
+            }
+        }
+        require(tagFound, "Tag not found");
+        URL[] memory result = new URL[](urlIds.length);
+        for (uint256 i = 0; i < urlIds.length; i++) {
+            result[i] = urls[urlIds[i]];
+        }
+        return result;
+    }
+    
+    function getAllURLs() public view returns (URL[] memory) {
+        return urls;
+    }
+    
+    function getURLsByUser(address userAddress) public view returns (URL[] memory) {
+        uint256[] memory userSubmittedURLs = users[userAddress].submittedURLs;
+        URL[] memory result = new URL[](userSubmittedURLs.length);
+        for (uint256 i = 0; i < userSubmittedURLs.length; i++) {
+            result[i] = urls[userSubmittedURLs[i]];
+        }
+        return result;
     }
 
-    // return urlArray length
-    function urlArray_length() public view returns (uint) {
-        return urlArray.length;
+    
+    function getURL(uint256 index) public view returns (URL memory) {
+        require(index < urls.length, "Invalid URL index");
+        return urls[index];
     }
-
-    // return element from urlArray
-    function urlArray_element(uint index) public view returns (string memory) {
-        return urlArray[index].url;
+    
+    function getTag(uint256 index) public view returns (Tag memory) {
+        require(index < tags.length, "Invalid Tag index");
+        return tags[index];
+    }
+    
+    function getUserUpvotedURLs(address userAddress) public view returns (URL[] memory) {
+        uint256[] memory userUpvotedURLs = users[userAddress].upvotedURLs;
+        URL[] memory result = new URL[](userUpvotedURLs.length);
+        for (uint256 i = 0; i < userUpvotedURLs.length; i++) {
+            result[i] = urls[userUpvotedURLs[i]];
+        }
+        return result;
+    }
+    
+    function getUserDownvotedURLs(address userAddress) public view returns (URL[] memory) {
+        uint256[] memory userDownvotedURLs = users[userAddress].downvotedURLs;
+        URL[] memory result = new URL[](userDownvotedURLs.length);
+        for (uint256 i = 0; i < userDownvotedURLs.length; i++) {
+            result[i] = urls[userDownvotedURLs[i]];
+        }
+        return result;
+    }
+    
+    function getUserSubmittedURLs(address userAddress) public view returns (URL[] memory) {
+        uint256[] memory userSubmittedURLs = users[userAddress].submittedURLs;
+        URL[] memory result = new URL[](userSubmittedURLs.length);
+        for (uint256 i = 0; i < userSubmittedURLs.length; i++) {
+            result[i] = urls[userSubmittedURLs[i]];
+        }
+        return result;
     }
 }
