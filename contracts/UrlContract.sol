63d3196d10b521b5d1e4938ca4bfd6467ad1a1e8
Nico Serrano <38594836+NicoSerranoP@users.noreply.github.com>
2023-05-11 18:58:55 -0500

    contracts(feat): submitURL + createTag optimization

--- contracts/UrlContract.sol
+++ contracts/UrlContract.sol
@@ -5,78 +5,122 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract UrlContract {
-    
+
     struct URL {
         string title;
         string url;
         address submittedBy;
         uint256 upvotes;
         uint256 downvotes;
-        string[] tagIds;
+        uint256[] tagIds;
     }
-    
+
     struct Tag {
         string name;
         address createdBy;
         uint256[] urlIds;
     }
-    
+
     struct User {
         uint256[] upvotedURLs;
         uint256[] downvotedURLs;
         uint256[] submittedURLs;
     }
-    
+
+    struct Tags {
+        Tag[] list;
+        mapping (string => uint256) names;
+    }
+
     URL[] private urls;
-    Tag[] private tags;
+    Tags private tags;
     mapping (address => User) private users;
-    
-    function createTagIfNotExists(string memory tagName) internal returns (uint256) {
-        for (uint256 i = 0; i < tags.length; i++) {
-            if (keccak256(bytes(tags[i].name)) == keccak256(bytes(tagName))) {
-                return i;
-            }
-        }
-        Tag memory newTag = Tag(tagName, msg.sender, new uint256[](0));
-        tags.push(newTag);
-        return tags.length - 1;
+    mapping (uint256 => uint256) public testing;
+
+    /// @notice Initialize contract with one tag
+    /// @dev Initialize with one tag to prevent the mapping of initial tag (0) to be confused as a empty value
+    /// @param firstTag First tag name
+    constructor (string memory firstTag) {
+        Tag memory newTag = Tag(firstTag, msg.sender, new uint256[](0));
+        tags.list.push(newTag);
     }
-    
-    function submitURL(string memory title, string memory url, string[] memory tagIds) public {
-        require(bytes(title).length > 0 && bytes(url).length > 0 && tagIds.length > 0, "Invalid input");
-        uint256[] memory tagIndexes = new uint256[](tagIds.length);
-        for (uint256 i = 0; i < tagIds.length; i++) {
-            tagIndexes[i] = createTagIfNotExists(tagIds[i]);
+
+    /// @notice Save URL in smart contract. Create tags if they don't exist
+    /// @dev The function flow is: get url id, create tags (and save url id), save url
+    /// @param title URL content title
+    /// @param url URL link
+    /// @param _tags Tag names list associated with URL.
+    function submitURL(string memory title, string memory url, string[] calldata _tags) public {
+        require(bytes(title).length > 0 && bytes(url).length > 0 && _tags.length > 0, "Invalid input");
+        // If tx fails then changes rollback. We can be sure url index in the beginning = url index in the end
+        uint256 urlIndex = urls.length;
+        // get tag indexes or create new tags. Also add url index to each tag
+        uint256[] memory tagIndexes = new uint256[](_tags.length);
+        for (uint256 i = 0; i < _tags.length; i++) {
+            tagIndexes[i] = createTagIfNotExists(_tags[i]);
+            addUrlToTag(tagIndexes[i], urlIndex);
         }
-        URL memory newURL = URL(title, url, msg.sender, 0, 0, tagIds);
+        // add new url to array
+        URL memory newURL = URL(title, url, msg.sender, 0, 0, tagIndexes);
         urls.push(newURL);
-        for (uint256 i = 0; i < tagIndexes.length; i++) {
-            tags[tagIndexes[i]].urlIds.push(urls.length - 1);
-        }
     }
-    
-    function getURLsByTag(string memory tagId) public view returns (URL[] memory) {
-        bool tagFound = false;
-        uint256[] memory urlIds = new uint256[](0);
-        for (uint256 i = 0; i < tags.length; i++) {
-            if (keccak256(bytes(tags[i].name)) == keccak256(bytes(tagId))) {
-                tagFound = true;
-                urlIds = tags[i].urlIds;
-                break;
-            }
+
+    /// @notice Get a tag id from array or add a new tag if it doesn't exist
+    /// @dev It is used inside submitURL but it should be available to be called alone
+    /// @param name Tag name
+    function createTagIfNotExists(string memory name) public returns (uint256){
+        // check if it is the first tag in the array
+        string memory firstTag = tags.list[0].name;
+        if (keccak256(bytes(name)) == keccak256(bytes(firstTag))){
+            return 0;
         }
-        require(tagFound, "Tag not found");
-        URL[] memory result = new URL[](urlIds.length);
-        for (uint256 i = 0; i < urlIds.length; i++) {
+        // check if tag is already registered
+        uint256 index = tags.names[name];
+        if (index == 0){
+            // add new tag to array
+            Tag memory newTag = Tag(name, msg.sender, new uint256[](0));
+            tags.list.push(newTag);
+            uint256 newIndex = tags.list.length - 1;
+            tags.names[name] = newIndex;
+            return newIndex;
+        }
+        return index;
+    }
+
+    /// @notice Save URL id in tag object so it is easily retrievable later
+    /// @dev It is an internal function used inside submitURL
+    /// @param index Tag id
+    /// @param url URL id
+    function addUrlToTag(uint256 index, uint256 url) internal {
+        tags.list[index].urlIds.push(url);
+    }
+
+    /// @notice Retrieve all URLs associated with a specific tag name
+    /// @param name Tag name
+    function getURLsByTag(string memory name) public view returns (URL[] memory) {
+        // check if it is the frist tag in the array
+        string memory firstTag = tags.list[0].name;
+        require(keccak256(bytes(name)) != keccak256(bytes(firstTag)), "This is the first tag. It is invalid");
+        // check that the tag exists
+        uint256 index = tags.names[name];
+        require(index != 0, "Tag not found");
+
+        uint256[] memory urlIds =  tags.list[index].urlIds;
+        uint256 length = urlIds.length;
+        URL[] memory result = new URL[](length);
+        for (uint256 i =0; i < length; i ++){
             result[i] = urls[urlIds[i]];
         }
         return result;
     }
-    
+
+    /// @notice Retrieve all registered URLs
     function getAllURLs() public view returns (URL[] memory) {
         return urls;
     }
-    
+
+    /// @notice Retrieve all URLs submitted by a specific user
+    /// @param userAddress User id
     function getURLsByUser(address userAddress) public view returns (URL[] memory) {
         uint256[] memory userSubmittedURLs = users[userAddress].submittedURLs;
         URL[] memory result = new URL[](userSubmittedURLs.length);
@@ -86,17 +130,22 @@ contract UrlContract {
         return result;
     }
 
-    
+    /// @notice Get a specific URL
+    /// @param index URL id
     function getURL(uint256 index) public view returns (URL memory) {
         require(index < urls.length, "Invalid URL index");
         return urls[index];
     }
-    
+
+    /// @notice Get a specific tag
+    /// @param index Tag id
     function getTag(uint256 index) public view returns (Tag memory) {
-        require(index < tags.length, "Invalid Tag index");
-        return tags[index];
+        require(index < tags.list.length, "Invalid Tag index");
+        return tags.list[index];
     }
-    
+
+    /// @notice Get all upvoted URLs of a specific user
+    /// @param userAddress User id
     function getUserUpvotedURLs(address userAddress) public view returns (URL[] memory) {
         uint256[] memory userUpvotedURLs = users[userAddress].upvotedURLs;
         URL[] memory result = new URL[](userUpvotedURLs.length);
@@ -105,7 +154,9 @@ contract UrlContract {
         }
         return result;
     }
-    
+
+    /// @notice Get all downvoted URLs of a specific user
+    /// @param userAddress User id
     function getUserDownvotedURLs(address userAddress) public view returns (URL[] memory) {
         uint256[] memory userDownvotedURLs = users[userAddress].downvotedURLs;
         URL[] memory result = new URL[](userDownvotedURLs.length);
@@ -114,7 +165,9 @@ contract UrlContract {
         }
         return result;
     }
-    
+
+    /// @notice Get all submitted URLs of a specific user
+    /// @param userAddress User id
     function getUserSubmittedURLs(address userAddress) public view returns (URL[] memory) {
         uint256[] memory userSubmittedURLs = users[userAddress].submittedURLs;
         URL[] memory result = new URL[](userSubmittedURLs.length);
