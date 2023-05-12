1c20f03e8f13e7b4d07154f14d26bc440e61193c
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-05-12 22:46:18 +0900

    fix: :iphone: Show logo on mobile

--- apps/poc-next/components/main-nav.tsx
+++ apps/poc-next/components/main-nav.tsx
@@ -13,9 +13,9 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex">
+      <Link href="/" className="flex items-center space-x-2 md:flex">
         <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">
+        <span className="font-bold sm:inline-block">
           {siteConfig.name}
         </span>
       </Link>
