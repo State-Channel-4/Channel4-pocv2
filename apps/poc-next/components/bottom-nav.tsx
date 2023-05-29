c39ba5e7705f90586ac13b5cb65eb143b4d45f6d
Nico Serrano <nnicosp@hotmail.com>
2023-05-29 17:11:31 -0500

    frontend(feat): navbar icon for subnavbar working

--- apps/poc-next/components/bottom-nav.tsx
+++ apps/poc-next/components/bottom-nav.tsx
@@ -52,8 +52,7 @@ export function BottomNavigation({ iconColor = "#D0D1D2" }: Props) {
             <RiHeartFill size="25px" fill={iconColor} />
           </Link>
 
-          <Link
-            href="#"
+          <button
             onClick={handleMenuClick}
             className="flex cursor-pointer items-center bg-transparent"
           >
@@ -62,7 +61,7 @@ export function BottomNavigation({ iconColor = "#D0D1D2" }: Props) {
             ) : (
               <RiMenu2Line size="25px" fill={iconColor} />
             )}
-          </Link>
+          </button>
         </div>
       </nav>
     </div>
--- apps/poc-next/config/site.ts
+++ apps/poc-next/config/site.ts
@@ -11,7 +11,7 @@ export const siteConfig = {
     },
     {
       title: "Add site",
-      href: "/sites/new",
+      href: "/submit-url",
     },
     {
       title: "Add tag",
