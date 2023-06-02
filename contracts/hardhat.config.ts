2565df626fd21ab297bc1f31f8efbeeca69c251b
Nico Serrano <nnicosp@hotmail.com>
2023-06-02 13:11:04 -0500

    contracts(feat): deploy to sepolia network

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://rpc2.sepolia.org`,
+      accounts: ['bd0cef31fef0e40085b0f2801744057feb0a6b3d3a7d96d01eac0c08eea2e596']
+    }
+  }
 };
 
 export default config;
