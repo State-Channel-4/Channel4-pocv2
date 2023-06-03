c01c3ad964f5a07bf20f2895e37dfd15e751b0e2
Nico Serrano <nnicosp@hotmail.com>
2023-06-02 23:05:47 -0500

    frontend(feat): save provider when creating the wallet

--- apps/poc-next/store/wallet.ts
+++ apps/poc-next/store/wallet.ts
@@ -1,9 +1,10 @@
-import { HDNodeWallet, Wallet, getDefaultProvider } from "ethers"
+import { HDNodeWallet, JsonRpcProvider, Wallet } from "ethers"
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
 
 type Store = {
   wallet: Wallet | HDNodeWallet | null
+  provider: JsonRpcProvider | null
   createWallet: (password: string) => Promise<string | null>
   updateWallet: (data: Wallet | HDNodeWallet | null) => void
 }
@@ -12,12 +13,13 @@ export const useWalletStore = create<Store>()(
   persist(
     (set, get) => ({
       wallet: null,
+      provider: null,
       createWallet: async (password) => {
         if (get().wallet) return null
-        const provider = getDefaultProvider("sepolia")
+        const provider = new JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL!)
         const wallet = Wallet.createRandom(provider)
         const encrypted = await wallet.encrypt(password)
-        set({ wallet: wallet })
+        set({ wallet: wallet, provider: provider })
         return encrypted
       },
       updateWallet: (data) => set({ wallet: data }),
