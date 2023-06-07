9d912b8db7db61c00ea625d255c8ab8f04a6d279
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-06-07 16:43:25 -0500

    refactor: :recycle: Refactor discover functions

--- apps/poc-next/app/discover/page.tsx
+++ apps/poc-next/app/discover/page.tsx
@@ -2,16 +2,16 @@
 
 import { useEffect, useRef, useState } from "react"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
 import { C4Content, TagMap } from "@/types"
 import { LinkIcon, ThumbsUpIcon } from "lucide-react"
-import useSWR, { Fetcher } from "swr"
+import useSWR from "swr"
 
 import { cn } from "@/lib/utils"
 import { Button, buttonVariants } from "@/components/ui/button"
 import TagList from "@/components/ui/tag-list"
 
 import SiteFrame from "./SiteFrame"
+import { feedbackMessages, getMix, updateLikesInApi } from "./utils"
 
 // TODO:
 // - [ ] Get links based on tags from API
@@ -20,36 +20,15 @@ import SiteFrame from "./SiteFrame"
 // - [x] Add open in new tab button
 // - [x] Add submitted by (?)
 // - [x] Add micro-interactions
-// - [ ] Add loading animations
-// - [ ] Add error handling
-
-const getMix: Fetcher<{ urls: C4Content[] }, TagMap> = (tags) => {
-  const tagQueries = new URLSearchParams()
-  tags.forEach((tag) => tagQueries.append("tags", tag._id))
-  return fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/mix?${tagQueries.toString()}`
-  ).then((response) => response.json())
-}
-
-const __testLink: C4Content = {
-  _id: "123",
-  url: "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik",
-  tags: [{ _id: "1", name: "test" }],
-  submittedBy: "--nightc0re--",
-  createdAt: new Date("2021-10-10T12:00:00.000Z"),
-  updatedAt: new Date("2021-10-10T12:00:00.000Z"),
-  title: "OpenStreetMap",
-  syncedToBlockchain: false,
-  likes: ["abc", "def"],
-}
+// - [x] Add loading animations
+// - [x] Add error handling
 
 const Discover = () => {
   const selectedTags = useRef<TagMap>(new Map())
   const [activeContent, setActiveContent] = useState<C4Content | null>(null)
   const [mixIndex, setMixIndex] = useState<number>(0)
   const [mixCompleted, setMixCompleted] = useState<boolean>(false)
-  const [userLiked, setUserLiked] = useState<boolean>(false)
-  const router = useRouter()
+  const [userLikes, setUserLikes] = useState<string[]>([])
 
   const { data, error, isLoading } = useSWR(selectedTags.current, getMix, {
     revalidateOnFocus: false,
@@ -58,30 +37,13 @@ const Discover = () => {
       const content = data.urls
       console.log("data", data)
       if (!content || content.length === 0) {
-        // router.push("/")
         setActiveContent(null)
         return
       }
-      // TODO: Remove test link
-      data.urls.push(__testLink)
+
       setActiveContent(content[mixIndex])
     },
   })
-
-  const feedbackMessages = {
-    "not-found": "No content found for the selected tags. Please try again.",
-    "no-tags": "No tags selected. Please select at least one tag.",
-    loading: "Loading content...",
-  }
-
-  const handleLike = (id: string) => {
-    if(!activeContent) return;
-    setUserLiked(!userLiked)
-    // this updates the UI optimistically
-    activeContent.likes = userLiked ? activeContent.likes - 1 : activeContent.likes + 1
-
-    // TODO: Update likes in API
-  }
 
   const getTagsFromStore = () => {
     const tagsFromStore = sessionStorage.getItem("c4.tags")
@@ -97,6 +59,21 @@ const Discover = () => {
     }
   }
 
+  const likeOrUnlikeActiveContent = (contentId: string) => {
+    if (!activeContent) return
+    if (userLikes.includes(contentId)) {
+      // TODO: Update the actual logged in user likes
+      setUserLikes((prev) => prev.filter((like) => like !== contentId))
+    } else {
+      setUserLikes((prev) => [...prev, contentId])
+    }
+    activeContent.likes = userLikes
+      ? activeContent.likes - 1
+      : activeContent.likes + 1
+
+    updateLikesInApi(contentId)
+  }
+
   const changeActiveContent = () => {
     if (!data) return
     setActiveContent(data.urls[mixIndex + 1])
@@ -110,16 +87,29 @@ const Discover = () => {
 
   useEffect(() => {
     getTagsFromStore()
+    // TODO: Intitialize user likes
   }, [])
 
   if (error) {
-    return <p className="mx-auto flex w-full items-center justify-center p-6">{feedbackMessages["not-found"]}</p>
+    return (
+      <p className="mx-auto flex w-full items-center justify-center p-6">
+        {feedbackMessages["not-found"]}
+      </p>
+    )
   }
   if (isLoading) {
-    return <p className="mx-auto flex w-full items-center justify-center p-6">{feedbackMessages.loading}</p>
+    return (
+      <p className="mx-auto flex w-full items-center justify-center p-6">
+        {feedbackMessages.loading}
+      </p>
+    )
   }
   if (!selectedTags.current || selectedTags.current.size === 0) {
-    return <p className="mx-auto flex w-full items-center justify-center p-6">{feedbackMessages["no-tags"]}</p>
+    return (
+      <p className="mx-auto flex w-full items-center justify-center p-6">
+        {feedbackMessages["no-tags"]}
+      </p>
+    )
   }
 
   return (
@@ -142,17 +132,19 @@ const Discover = () => {
             <div className="flex items-center gap-2">
               <button
                 className="bg-primary/20 hover:bg-primary/10 text-primary/70 group inline-flex items-center gap-2 self-start rounded-full px-4 py-2 text-sm transition-all duration-300"
-                onClick={(e) => handleLike(activeContent._id)}
+                onClick={(e) => likeOrUnlikeActiveContent(activeContent._id)}
               >
                 <ThumbsUpIcon
                   size={16}
                   className={cn(
                     "transition-all duration-300 group-hover:-rotate-12 group-hover:scale-110 group-hover:text-yellow-300",
-                    userLiked && "text-yellow-300"
+                    userLikes.includes(activeContent._id) && "text-yellow-300"
                   )}
                   aria-label="Like"
                 />
-                <p className="text-primary">{userLiked ? "Liked" : "Like"}</p>
+                <p className="text-primary">
+                  {userLikes.includes(activeContent._id) ? "Liked" : "Like"}
+                </p>
                 <p>•</p>
                 <p>{activeContent.likes}</p>
               </button>
@@ -169,7 +161,7 @@ const Discover = () => {
             <p className="p-1"></p>
             <Button
               className={cn(
-                buttonVariants({ size: "lg" , variant: 'default'}),
+                buttonVariants({ size: "lg", variant: "default" }),
                 "rounded-full font-bold uppercase transition-all duration-500 active:scale-75"
               )}
               disabled={mixCompleted}
@@ -187,7 +179,7 @@ const Discover = () => {
         {selectedTags.current.size > 0 && (
           <>
             <p className="text-primary text-xs uppercase tracking-widest">
-              showing items with
+              viewing shows from
             </p>
             <TagList tags={selectedTags.current} />
             <Link href="/" passHref>
